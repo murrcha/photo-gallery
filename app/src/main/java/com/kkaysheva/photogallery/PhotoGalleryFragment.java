0e056c70ebbfd8011dc49a0889eac14a9deeb4e5
Ksenya Kaysheva <murrcha@me.com>
2018-11-08 11:01:23 +0400

    init project

---
+++ app/src/main/java/com/kkaysheva/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,35 @@
+package com.kkaysheva.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PhotoGalleryFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        recyclerView = view.findViewById(R.id.photo_recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return view;
+    }
+}
